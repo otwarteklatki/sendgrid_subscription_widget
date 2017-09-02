a6027ceefcbb7af7df37aa6d68bd41737f00f409
Piotr Arendt <peter@airnauts.com>
2017-09-02 19:19:14 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 // Change the url to the domain of your app
 exports.url = 'http://localhost:3090';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "info@nahtamatudloomad.ee";
+exports.senderName = "Nähtamatud Loomad";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
+exports.listId = '1871379';
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
@@ -14,4 +14,4 @@ exports.templateId = null;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
-exports.notificationEmail = "admin@example.com";
+exports.notificationEmail = "nahtamatudloomad@mailinator.com";
