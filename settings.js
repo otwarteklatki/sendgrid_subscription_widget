9ab93cff91e43cbe6d662bbc1d6a25b15ea8cfe9
Ville Sokk <ville.sokk@gmail.com>
2017-10-08 19:41:19 +0300

    Changed list ID to the production one.

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ exports.senderName = "Nähtamatud Loomad";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = '1871379';
+exports.listId = '1670859';
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
